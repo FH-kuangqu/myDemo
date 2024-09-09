d7c9837f6447ad3d1387ea5b679b2d09656846ac
FH-kuangqu <2791538089@qq.com>
2024-09-09 14:48:22 +0800

    轮播图路径修改

--- 轮播图demo/js/index.js
+++ 轮播图demo/js/index.js
@@ -10,10 +10,10 @@ var nextImg = document.querySelector('.next img');
 
 
 var arr = [
-    "../images/1.png",
-    "../images/2.png",
-    "../images/3.png",
-    "../images/4.png"
+    "./images/1.png",
+    "./images/2.png",
+    "./images/3.png",
+    "./images/4.png"
 ]
 
 var index = 0;
