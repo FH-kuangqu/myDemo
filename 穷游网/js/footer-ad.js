cf77b46b8abb94cbcf147480d1b6e2141067b3a7
粪海狂蛆 <129647908+FH-kuangqu@users.noreply.github.com>
2024-09-09 14:52:51 +0800

    仓库内文件路径修改

--- 穷游网/js/footer-ad.js
+++
@@ -1,6 +0,0 @@
-let ad = document.querySelector(".footer-ad");
-let adNone = document.getElementById("ad-none");
-
-adNone.addEventListener("click", function() {
-    ad.style.display = "none";
-});
